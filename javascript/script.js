4451c1e86acc73c2ac6e98f8f0aeac3b63e74a46
khusainovabduboriy <83864380+khusainovabduboriy@users.noreply.github.com>
2021-08-30 14:05:46 +0500

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+$(function(){
+    $('.burger').click(function(){
+        $('.burger').toggleClass('burger_active');
+    });
+    $('.burger').click(function(){
+        $('.menu_ul').toggleClass('active');
+    });
+    $('.menu_ul').click(function(){
+        $('.menu_ul').removeClass('active');
+        $('.burger').removeClass('burger_active');
+    });
+    $('.previ').hover(function(){
+        $('.icon1').toggleClass('color');
+    });
+    $('.next').hover(function(){
+        $('.icon2').toggleClass('color');
+    });
+
+});
